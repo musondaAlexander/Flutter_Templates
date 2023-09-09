0f1cdf34cf30c7ef9eddbbff37f3b05cd7267652
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2023-09-09 14:51:48 +0200

    addded a srowview to both pages

--- practiceapplication/lib/login.dart
+++ practiceapplication/lib/login.dart
@@ -15,94 +15,96 @@ class LoginScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
         child: Center(
-          child: Column(children: [
-            const SizedBox(height: 50),
-            Image.asset('assets/images/logoicon.ico', height: 60, width: 60),
-            const SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 10,
-                          offset: Offset(0, 10))
-                    ]),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text('Welcome back ',
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.black,
-                            )),
-                        const Text(
-                            "Enter your credentials to access your account",
-                            style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black,
-                                fontFamily: AutofillHints.addressState)),
-                        const SizedBox(height: 20),
-                        const SizedBox(
-                          width: 300,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              icon: Icon(Icons.email),
-                              hintText: 'Email',
-                            ),
-                          ),
-                        ),
-                        // INOUT FIELD FOR PASSWORD
-                        const SizedBox(height: 20),
-                        const SizedBox(
-                          width: 300,
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              icon: Icon(Icons.lock),
-                              hintText: 'Password',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        SizedBox(
-                          width: 300,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(
-                                  const Color.fromARGB(255, 85, 176, 250)),
-                              foregroundColor:
-                                  MaterialStateProperty.all(Colors.white),
-                            ),
-                            child: const Text('Sign In'),
-                          ),
-                        ),
+          child: SingleChildScrollView(
+            child: Column(children: [
+              const SizedBox(height: 50),
+              Image.asset('assets/images/logoicon.ico', height: 60, width: 60),
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 10,
+                            offset: Offset(0, 10))
                       ]),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text('Welcome back ',
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.black,
+                              )),
+                          const Text(
+                              "Enter your credentials to access your account",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontFamily: AutofillHints.addressState)),
+                          const SizedBox(height: 20),
+                          const SizedBox(
+                            width: 300,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.email),
+                                hintText: 'Email',
+                              ),
+                            ),
+                          ),
+                          // INOUT FIELD FOR PASSWORD
+                          const SizedBox(height: 20),
+                          const SizedBox(
+                            width: 300,
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.lock),
+                                hintText: 'Password',
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: 300,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    const Color.fromARGB(255, 85, 176, 250)),
+                                foregroundColor:
+                                    MaterialStateProperty.all(Colors.white),
+                              ),
+                              child: const Text('Sign In'),
+                            ),
+                          ),
+                        ]),
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 20),
-            // Text button for forgot password
-            TextButton(
-              onPressed: () {},
-              child: const Text('Forgot Password?'),
-            ),
-            const SizedBox(height: 20),
-            // Text button for dont have an account
-            TextButton(
-              onPressed: () {
-                Get.to(() => SignApp());
-              },
-              child: const Text('Don\'t have an account? Sign up'),
-            ),
-          ]),
+              const SizedBox(height: 20),
+              // Text button for forgot password
+              TextButton(
+                onPressed: () {},
+                child: const Text('Forgot Password?'),
+              ),
+              const SizedBox(height: 20),
+              // Text button for dont have an account
+              TextButton(
+                onPressed: () {
+                  Get.to(() => SignApp());
+                },
+                child: const Text('Don\'t have an account? Sign up'),
+              ),
+            ]),
+          ),
           // Text button for forgot password
         ),
       ),
--- practiceapplication/lib/signapp.dart
+++ practiceapplication/lib/signapp.dart
@@ -18,105 +18,109 @@ class SignApp extends StatelessWidget {
       body: Padding(
           padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
           child: Center(
-            child: Column(children: [
-              const SizedBox(height: 50),
-              Image.asset('assets/images/logoicon.ico', height: 60, width: 60),
-              const SizedBox(height: 40),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 10,
-                            offset: Offset(0, 10))
-                      ]),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Text('Welcome',
-                              style: TextStyle(
-                                fontSize: 25,
-                                color: Colors.black,
-                              )),
-                          const Text("Enter your credentials to create account",
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.black,
-                                  fontFamily: AutofillHints.addressState)),
-                          const SizedBox(height: 20),
-                          SizedBox(
-                            width: 300,
-                            child: TextField(
-                              controller: emailController,
-                              decoration: InputDecoration(
-                                icon: Icon(Icons.email),
-                                hintText: 'Email',
-                              ),
-                            ),
-                          ),
-                          // INPUT FIELD FOR PASSWORD
-                          const SizedBox(height: 20),
-                          SizedBox(
-                            width: 300,
-                            child: TextField(
-                              controller: passwordController,
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                icon: Icon(Icons.lock),
-                                hintText: 'Password',
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          SizedBox(
-                            width: 300,
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    const Color.fromARGB(255, 85, 176, 250)),
-                                foregroundColor:
-                                    MaterialStateProperty.all(Colors.white),
-                              ),
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      content: Text(emailController.text),
-                                    );
-                                  },
-                                );
-                              },
-                              child: Text('Sign Up'),
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          // SizedBox(
-                          //   width: 300,
-                          //   child: ElevatedButton(
-                          //     onPressed: () {
-                          //       Get.to(LoginScreen());
-                          //     },
-                          //     child: Text('login'),
-                          //   ),
-                          // ),
+            child: SingleChildScrollView(
+              child: Column(children: [
+                const SizedBox(height: 50),
+                Image.asset('assets/images/logoicon.ico',
+                    height: 60, width: 60),
+                const SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 10,
+                              offset: Offset(0, 10))
                         ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text('Welcome',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.black,
+                                )),
+                            const Text(
+                                "Enter your credentials to create account",
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black,
+                                    fontFamily: AutofillHints.addressState)),
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: 300,
+                              child: TextField(
+                                controller: emailController,
+                                decoration: InputDecoration(
+                                  icon: Icon(Icons.email),
+                                  hintText: 'Email',
+                                ),
+                              ),
+                            ),
+                            // INPUT FIELD FOR PASSWORD
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: 300,
+                              child: TextField(
+                                controller: passwordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  icon: Icon(Icons.lock),
+                                  hintText: 'Password',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: 300,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      const Color.fromARGB(255, 85, 176, 250)),
+                                  foregroundColor:
+                                      MaterialStateProperty.all(Colors.white),
+                                ),
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        content: Text(emailController.text),
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Text('Sign Up'),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            // SizedBox(
+                            //   width: 300,
+                            //   child: ElevatedButton(
+                            //     onPressed: () {
+                            //       Get.to(LoginScreen());
+                            //     },
+                            //     child: Text('login'),
+                            //   ),
+                            // ),
+                          ]),
+                    ),
                   ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Get.to(() => LoginScreen());
-                },
-                child: const Text('Already have an account? Login'),
-              ),
-            ]),
+                TextButton(
+                  onPressed: () {
+                    Get.to(() => LoginScreen());
+                  },
+                  child: const Text('Already have an account? Login'),
+                ),
+              ]),
+            ),
           )),
     );
   }
